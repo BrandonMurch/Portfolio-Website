48542fa9af0a4b4fcbc887c1e3adfd80280582d1
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-06-12 14:38:00 +0300

    Responsive design with React

--- src/about_me.js
+++ src/about_me.js
@@ -8,8 +8,9 @@ class AboutMe extends React.Component {
     render() {
         // TODO add a more detailed intro
         const aboutMe = "a software developer living in tasmania.";
+        const background = this.props.state.orientation === "landscape" ? styles.landscapeBackground : styles.portraitBackground;
         return (
-            <section id="aboutme" className={styles.landscapeBackground}>
+            <section id="aboutme" className={background}>
                     <div className={styles.container}>
                         <h1 className={styles.title}> brandon murch </h1>
                         <h3 className={styles.subtitle}> {aboutMe} </h3>
--- src/app.js
+++ src/app.js
@@ -1,17 +1,65 @@
 import React from 'react';
-import './app.css';
+import styles from './app.module.css';
 import NavBar from './navbar';
 import AboutMe from './about_me';
-import Contact from './contact';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            orientation: "landscape",
+            size: "large",
+        };
+        this.updateResize = this.updateResize.bind(this);
+    }
+
+
+    componentDidMount() {
+        this.updateResize();
+        window.addEventListener("resize", this.updateResize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateResize);
+    }
+
+    updateResize() {
+        console.log("resized");
+        this.setState({
+            orientation: getOrientation(),
+            size: getSize(),
+        })
+        console.log(this.state.orientation);
+        console.log(this.state.size);
+    }
+
     render() {
         return (
-            <div className="app">
+            <div className={styles.app}>
                 <NavBar />
-                <AboutMe />
+                <AboutMe state={this.state}/>
             </div>
         )
+    }
+}
+
+
+function getOrientation() {
+    if (window.innerHeight > window.innerWidth) {
+        return "portrait";
+    } else {
+        return "landscape";
+    }
+}
+
+function getSize() {
+    const width = window.innerWidth;
+    if (width > 1280) {
+        return "large";
+    } else if (width > 600) {
+        return " medium";
+    } else {
+        return "small";
     }
 }
 
