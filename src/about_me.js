71960dcf74324652b4d13fd7a3e21d315c6f40eb
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-06-16 13:28:45 +0300

    Responsive design moved to Media queries.

--- src/about_me.js
+++ src/about_me.js
@@ -8,13 +8,12 @@ class AboutMe extends React.Component {
     render() {
         // TODO add a more detailed intro
         const aboutMe = "a software developer living in tasmania.";
-        const background = this.props.state.orientation === "landscape" ? styles.landscapeBackground : styles.portraitBackground;
         return (
-            <section id="aboutme" className={background}>
-                    <div className={styles.container}>
-                        <h1 className={styles.title}> brandon murch </h1>
-                        <h3 className={styles.subtitle}> {aboutMe} </h3>
-                    </div>
+            <section id="aboutme" className={styles.background}>
+                <div className={styles.container}>
+                    <h1 className={styles.title}> brandon murch </h1>
+                    <h3 className={styles.subtitle}> {aboutMe} </h3>
+                </div>
             </section>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import './responsive.css'
 // import styles from './index.module.css';
 
 ReactDOM.render(
