7e239e51a64f16294d246f094f2c82de285384d3
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-06-22 04:54:41 +0300

    Finished designing contact page.

--- src/contact.js
+++ src/contact.js
@@ -4,22 +4,25 @@ import styles from './contact.module.css';
 class Contact extends React.Component {
     render() {
         return (
-            <form className={styles.contactForm}>
-                <div className={styles.inputGroup}>
-                    <label className={styles.label} htmlFor="name">Name</label><br />
-                    <input className={styles.textInput} type="text" id="name"></input>
-                </div>
-                <div className={styles.inputGroup}>
-                    <label className={styles.label} htmlFor="email">Email</label><br />
-                    <input className={styles.textInput}  type="email" id="email"></input>
-                </div>
-                <label className="text" >message</label>
-                <textarea className={styles.textInput} id="message" name="message"></textarea>
-                {
-                    // TODO bootstrap to css modules?
-                }
-                <input className="btn btn-secondary contact-submit" type="submit" value="Submit"></input>
-            </form>
+            <div className={styles.background}>
+                <form className={styles.contactForm}>
+                    <div className={styles.inputGroup}>
+                        <label className={styles.label} htmlFor="name">Name</label><br />
+                        <input className={styles.textInput} type="text" id="name"></input>
+                    </div>
+                    <div className={styles.inputGroup}>
+                        <label className={styles.label} htmlFor="email">Email</label><br />
+                        <input className={styles.textInput}  type="email" id="email"></input>
+                    </div>
+                    <div className={styles.wideGroup}>
+                        <label className={styles.label} >message</label>
+                        <textarea className={styles.textArea} id="message" name="message"></textarea>
+                    </div>
+                    <div className={styles.buttonContainer}>
+                        <input className={"btn btn-secondary " + styles.submit}  type="submit" value="Submit"></input>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
