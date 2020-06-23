ddae7ba6792e00ab2585716bf9c16ca232200826
BrandonMurch <36788304+BrandonMurch@users.noreply.github.com>
2020-06-23 13:48:49 +0300

    Update build

---
+++ precache-manifest.0415e67a4ab532c835c1a2c1e9f56c99.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e15bc02f67dc26fb3b2b0db7503826",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "9b7466af006b70b54217",
+    "url": "/build/static/css/main.917253fa.chunk.css"
+  },
+  {
+    "revision": "69d09697db97da3ebf16",
+    "url": "/build/static/js/2.6be7ab6c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/build/static/js/2.6be7ab6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7466af006b70b54217",
+    "url": "/build/static/js/main.2584c0c4.chunk.js"
+  },
+  {
+    "revision": "9639f41719903ebd67ee",
+    "url": "/build/static/js/runtime-main.10f07595.js"
+  },
+  {
+    "revision": "11eea593f74369faa88474b5e3aab839",
+    "url": "/build/static/media/Javascript Calculator.11eea593.png"
+  },
+  {
+    "revision": "586d5ecc3d69f4b79d7c56730b518970",
+    "url": "/build/static/media/Personal_Photo_cropped.586d5ecc.jpg"
+  },
+  {
+    "revision": "a8965fe24d64ce61b4d71cf483fd2be5",
+    "url": "/build/static/media/Personal_Photo_gradient.a8965fe2.jpg"
+  },
+  {
+    "revision": "91e82ee10289cea7bf4fb5d2a448484d",
+    "url": "/build/static/media/Pomodoro Clock.91e82ee1.png"
+  },
+  {
+    "revision": "ae1af67d584a87a1dbf9229589d969d9",
+    "url": "/build/static/media/QuoteGenerator.ae1af67d.png"
+  },
+  {
+    "revision": "824f8052d4ce521caaa05b5edadf4e40",
+    "url": "/build/static/media/Simon Says.824f8052.png"
+  },
+  {
+    "revision": "f14644910577f713a0cdb1db6b221ac7",
+    "url": "/build/static/media/amalficoast.f1464491.jpeg"
+  },
+  {
+    "revision": "17a749f3b54d8907538031795efd1ad0",
+    "url": "/build/static/media/hillsborough.17a749f3.jpeg"
+  },
+  {
+    "revision": "4ee994899c4ef42fbcf8059fc6fd3f5e",
+    "url": "/build/static/media/mtwellington.4ee99489.jpeg"
+  }
+]);
