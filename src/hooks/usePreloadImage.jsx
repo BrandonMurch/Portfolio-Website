41f671b39aa4e901b08e6d8d653a508279faa8cc
Brandon Murch <brandon.murch@protonmail.com>
2021-12-07 00:58:22 +1100

    Add ability to preload multiple images

--- src/hooks/usePreloadImage.jsx
+++ src/hooks/usePreloadImage.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 
-function preloadImage(backgroundSource, setLoaded) {
+function preloadImage(backgroundSource, onLoad) {
     const backgroundImage = new Image();
     backgroundImage.onloadend = () => {
         // Store image in window object to encourage some browsers to cache the image, and use it to prevent loading screen
         window[backgroundSource] = backgroundImage;
-        setLoaded(true);
+        onLoad();
     };
     backgroundImage.src = backgroundSource;
 }
@@ -15,11 +15,33 @@ function usePreloadImage(backgroundSource) {
     const [loaded, setLoaded] = useState(window[backgroundSource] !== undefined);
 
     if (!loaded) {
-        preloadImage(backgroundSource, setLoaded);
+        preloadImage(backgroundSource, () => setLoaded(true));
     }
 
     return loaded;
 
+}
+
+
+export function usePreloadImages(backgroundSources) {
+    const [unloadedImages, setUnloadedImages] = useState(backgroundSources.length);
+
+    if (unloadedImages > 0) {
+        for (let backgroundSource of backgroundSources) {
+            if (!window[backgroundSource]) {
+                preloadImage(
+                    backgroundSource, 
+                    () => setUnloadedImages((previousState) => previousState - 1)
+                );
+            } else  {
+                setUnloadedImages((previousState) => previousState - 1)
+            }
+  
+        }
+    } 
+
+
+    return unloadedImages <= 0
 }
 
 export default usePreloadImage;
