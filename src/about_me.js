f58566f4e8b5fed0a3bc415dfd5ac23222860516
Brandon Murch <brandon.murch@protonmail.com>
2021-12-06 11:48:27 +1100

    Add Loading screen to about me
    Loading screen is a placeholder while images load for a more seamless
    experience.

--- src/about_me.js
+++ src/about_me.js
@@ -1,22 +1,47 @@
 // Libraries
-import React from 'react';
+import React, { useState } from 'react';
 
 // CSS
 import styles from './about_me.module.css';
 
-class AboutMe extends React.Component {
-    render() {
-        // TODO add a more detailed intro
-        const aboutMe = "a software developer living in tasmania.";
-        return (
-            <section className={styles.background}>
-                <div className={styles.container}>
-                    <h1 className={styles.title}> brandon murch </h1>
-                    <h3 className={styles.subtitle}> {aboutMe} </h3>
-                </div>
-            </section>
-        )
-    }
+// Components
+import Loading from './loading_page';
+
+
+function preloadImage(backgroundSource, setLoaded){
+    const backgroundImage = new Image();
+    backgroundImage.onloadend = () => setLoaded(true);
+    backgroundImage.src = backgroundSource;
+    window[backgroundSource] = backgroundImage;
 }
+
+function AboutMe() {
+    const [loaded, setLoaded] = useState(false);
+    const aboutMe = "a software developer living in tasmania.";
+    const backgroundSource = window.innerHeight > window.innerWidth ? "images/Personal_Photo_cropped.jpg" : "images/Personal_Photo_gradient.jpg"
+    
+    if (!loaded) {
+        preloadImage(backgroundSource, setLoaded);
+    }
+
+
+
+    return (
+        <>
+        <div hidden={loaded}>
+            <Loading/>
+        </div>
+
+            <section hidden={!loaded} className={styles.background} style={{ backgroundImage:'url(' + backgroundSource + ')'}}>
+            <div className={styles.container}>
+                <h1 className={styles.title}> brandon murch </h1>
+                <h3 className={styles.subtitle}> {aboutMe} </h3>
+            </div>
+        </section>
+        </>
+    )
+
+}
+
 
 export default AboutMe;
