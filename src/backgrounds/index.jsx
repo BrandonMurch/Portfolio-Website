90d50549330c3e070a72d339a3378dcfc4201ebb
Brandon Murch <brandon.murch@protonmail.com>
2021-12-08 01:48:48 +1100

    Add background sections

---
+++ src/backgrounds/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './backgrounds.module.css';
+
+export function Background1() {
+    return (
+        <div className={styles.background1}/>
+    )
+}
+
+export function Background2() {
+    return (
+        <div className={styles.background2}/>
+    )
+}
