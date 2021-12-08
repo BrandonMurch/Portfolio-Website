651454ab8120f3b7c6165fd2d84bb4f31dad05b0
Brandon Murch <brandon.murch@protonmail.com>
2021-12-09 03:03:04 +1100

    Add irregular boundary to about
    Write about me. Work in progress.

--- src/about/index.jsx
+++ src/about/index.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import styles from "./about.module.css";
+import BoundarySVG from "../boundary";
 
 function About() {
 	return (
 		<section className={styles.background} id="about">
+			<BoundarySVG className={styles.boundary} />
 			<h1 className={styles.title}>About Me</h1>
 			<p className={styles.body}>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Necessitatibus sunt totam eveniet distinctio exercitationem sit
-				voluptatum aperiam quia harum cupiditate autem earum voluptate
-				ipsam, tenetur minima eaque. Qui, magnam accusantium.
+				From a chef, serving up delicacies all over the world, to a web
+				developer cooking up some sweet websites and solutions for your
+				business. Currently working towards my Bachelor of Computer
+				Science at Deakin Univeristy. Spending my spare time soaking up
+				all the new coding techniques and algorithms that I can.
 			</p>
+			<BoundarySVG className={styles.bottomBoundary} />
 		</section>
 	);
 }
---
+++ src/boundary.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function BoundarySVG(props) {
+	return (
+		<svg
+			className={props.className}
+			// width="1200"
+			// height="238"
+			viewBox="0 0 317.49999 62.970873"
+			version="1.1"
+			id="svg2620"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<defs id="defs2617" />
+			<g id="layer1">
+				<path
+					d="m 1.1108197,46.972873 c 0,0 42.4952073,-64.899978 84.2757833,-39.5170753 C 127.16717,32.838702 159.47364,51.588082 215.09087,51.284586 270.70811,50.981093 318.14443,50.583409 318.14443,50.583409 l -0.0867,8.627917 -317.97550243,1.052512 z"
+					id="path2714"
+				/>
+			</g>
+		</svg>
+	);
+}
+
+export default BoundarySVG;
