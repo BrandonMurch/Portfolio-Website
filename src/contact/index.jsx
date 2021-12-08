fdad03ed20e16348df846227afef86486ee7a9b0
Brandon Murch <brandon.murch@protonmail.com>
2021-12-08 21:27:23 +1100

    Add contact reset button

--- src/contact/index.jsx
+++ src/contact/index.jsx
@@ -1,110 +1,146 @@
-import React, {useState, useRef } from 'react';
-import styles from './contact.module.css';
+import React, { useState, useRef } from "react";
+import styles from "./contact.module.css";
 
 function Contact() {
-    const backgroundSource = "../images/mtwellington.jpeg";
-    const [submitted, setSubmitted] = useState(false);
-    const submitButton = useRef();
-    const defaultValues = {
-        name: "",
-        email: "",
-        message: "",
-    }
-    const [formContent, setFormContent] = useState(defaultValues);
+	const backgroundSource = "../images/mtwellington.jpeg";
+	const [submitted, setSubmitted] = useState(false);
+	const submitButton = useRef();
+	const defaultValues = {
+		name: "",
+		email: "",
+		message: "",
+	};
+	const [formContent, setFormContent] = useState(defaultValues);
 
-    function handleChange(event) {
-        // event.persist() must be used because setFormContent will execute asynchronously.
-        event.persist();
-        setFormContent((previousState) => {
-            return {...previousState, [event.target.name]: event.target.value}
-        }); 
-    }
+	function handleChange(event) {
+		// event.persist() must be used because setFormContent will execute asynchronously.
+		event.persist();
+		setFormContent((previousState) => {
+			return {
+				...previousState,
+				[event.target.name]: event.target.value,
+			};
+		});
+	}
 
+	const encode = (data) => {
+		return Object.keys(data)
+			.map(
+				(key) =>
+					encodeURIComponent(key) +
+					"=" +
+					encodeURIComponent(data[key])
+			)
+			.join("&");
+	};
 
-  const encode = (data) => {
-    return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-  }
+	function handleSubmit(event) {
+		submitButton.current.disabled = true;
+		fetch("/", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: encode({ "form-name": "contact", ...formContent }),
+		})
+			.then((response) => {
+				submitButton.current.disabled = false;
+				if (response.status === 200) {
+					alert("Success!");
+				} else
+					alert("Message was unsuccessful, please try again later.");
+			})
+			.catch((error) => alert(error));
+		event.preventDefault();
+	}
 
-    function handleSubmit(event) {
-        submitButton.current.disabled = true;
-        fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "contact", ...formContent })
-      })
-        .then((response) => {
-            submitButton.current.disabled = false;
-            if (response.status === 200) {
-                alert("Success!");
-            }
-            else 
-                alert("Message was unsuccessful, please try again later.");
-
-        }).catch(error => alert(error));        
-        event.preventDefault();
-    }
-
-    return (
-
-        <>
-        {/* <div hidden={loaded}>
+	return (
+		<>
+			{/* <div hidden={loaded}>
             <Loading/>
         </div> */}
 
-        <section 
-            id="contact"
-            className={styles.contact} 
-            style={{ 
-                backgroundImage:'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.3)), url(' + backgroundSource + ')', 
-            }}
-        >
-            <form 
-                name="contact"
-                className={
-                    styles.contactForm 
-                    + " " 
-                    + (submitted ? styles.submitted : "")
-                }
-                onSubmit={handleSubmit}
-            >
-                <input type="hidden" name="form-name" value="contact" />
-                <p hidden>
-                    <label>
-                        This field will stop bots. Don't fill it out.
-                        <input name="bot-field" />
-                    </label>
-                </p>
-                <TextInput name="name" type="text" onChange={handleChange}/>
-                <TextInput name="email" type="email" onChange={handleChange} />
-                <div className={styles.inputGroup}>
-                    <label className={styles.label}>message</label>
-                    <textarea className={styles.textArea} id="message" name="message" onChange={handleChange} required/>
-                </div>
-                <div className={styles.buttonContainer}>
-                    <input className={styles.submit}  type="submit" value="Submit" ref={submitButton} onClick={() => setSubmitted(true)}/>
-                </div>
-            </form>
-        </section>
-        </>
-    )
+			<section
+				id="contact"
+				className={styles.contact}
+				style={{
+					backgroundImage:
+						"linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.3)), url(" +
+						backgroundSource +
+						")",
+				}}
+			>
+				<form
+					name="contact"
+					className={
+						styles.contactForm +
+						" " +
+						(submitted ? styles.submitted : "")
+					}
+					onSubmit={handleSubmit}
+				>
+					<input type="hidden" name="form-name" value="contact" />
+					<p hidden>
+						<label>
+							This field will stop bots. Don't fill it out.
+							<input name="bot-field" />
+						</label>
+					</p>
+					<TextInput
+						name="name"
+						type="text"
+						onChange={handleChange}
+					/>
+					<TextInput
+						name="email"
+						type="email"
+						onChange={handleChange}
+					/>
+					<div className={styles.inputGroup}>
+						<label className={styles.label}>message</label>
+						<textarea
+							className={styles.textArea}
+							id="message"
+							name="message"
+							onChange={handleChange}
+							required
+						/>
+					</div>
+					<div className={styles.buttonContainer}>
+						<input
+							className={styles.submit}
+							type="reset"
+							value="nevermind"
+						/>
+						<input
+							className={styles.submit}
+							type="submit"
+							value="Send Brandon an email!"
+							ref={submitButton}
+							onClick={() => setSubmitted(true)}
+						/>
+					</div>
+				</form>
+			</section>
+		</>
+	);
 }
 
-
 function TextInput(props) {
-    return (
-        <div className={styles.inputGroup}>
-            <label 
-                className={styles.label} 
-                htmlFor={props.name}
-            >
-                {props.name}
-            </label>
-            <br />
-            <input className={styles.textInput} type={props.type}  id={props.name} name={props.name} onChange={props.onChange} required/>
-        </div>
-    )
+	return (
+		<div className={styles.inputGroup}>
+			<label className={styles.label} htmlFor={props.name}>
+				{props.name}
+			</label>
+			<br />
+			<input
+				className={styles.textInput}
+				type={props.type}
+				id={props.name}
+				name={props.name}
+				onChange={props.onChange}
+				required
+			/>
+		</div>
+	);
 }
 
 export default Contact;
