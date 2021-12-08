8e9b511bc39324bf00812ed5b3e9cc3ca2922151
Brandon Murch <brandon.murch@protonmail.com>
2021-12-09 03:11:47 +1100

    Add background to logo

--- src/home/logo.jsx
+++ src/home/logo.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import styles from './home.module.css'
+import React from "react";
+import styles from "./home.module.css";
 
 function Logo() {
-    return ( 
-        <div className={styles.logoContainer}>
-            <a className={styles.logoText} href="/#">bm</a>
-        </div>
-     );
+	return (
+		<div className={styles.logoContainer}>
+			<div className={styles.logoBackground}></div>
+			<a className={styles.logoText} href="/#">
+				bm
+			</a>
+		</div>
+	);
 }
 
 export default Logo;
