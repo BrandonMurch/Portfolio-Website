fd46123899e754eafac30ddc297714f55d1d6fee
Brandon Murch <brandon.murch@protonmail.com>
2021-12-08 19:39:16 +1100

    Cleanup code

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,70 +1,70 @@
-import React from 'react';
-import NavBar from '../navbar';
-import Home from '../home';
-import About from '../about';
-import Contact from '../contact';
-import Projects from '../projects';
-import {Background1} from '../backgrounds';
+import React from "react";
+import NavBar from "../navbar";
+import Home from "../home";
+import About from "../about";
+import Contact from "../contact";
+import Projects from "../projects";
+import { Background1 } from "../backgrounds";
+import styles from "./app.module.css";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            orientation: "landscape",
-            size: "large",
-        };
-        this.updateResize = this.updateResize.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			orientation: "landscape",
+			size: "large",
+		};
+		this.updateResize = this.updateResize.bind(this);
+	}
 
-    componentDidMount() {
-        this.updateResize();
-        window.addEventListener("resize", this.updateResize);
-    }
+	componentDidMount() {
+		this.updateResize();
+		window.addEventListener("resize", this.updateResize);
+	}
 
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.updateResize);
-    }
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.updateResize);
+	}
 
-    updateResize() {
-        this.setState({
-            orientation: getOrientation(),
-            size: getSize(),
-        })
-    }
+	updateResize() {
+		this.setState({
+			orientation: getOrientation(),
+			size: getSize(),
+		});
+	}
 
-    render() {
-        return (
-            <>
-                <NavBar />
-                <Home />
-                <About />
-                <Background1 />
-                <Projects />
-                {/* <Background2 /> */}
-                <Contact />
-            </>
-        )
-    }
+	render() {
+		return (
+			<div className={styles.appWrapper}>
+				<NavBar />
+				<Home />
+				<About />
+				<Background1 />
+				<Projects />
+				{/* <Background2 /> */}
+				<Contact />
+			</div>
+		);
+	}
 }
 
-
 function getOrientation() {
-    if (window.innerHeight > window.innerWidth) {
-        return "portrait";
-    } else {
-        return "landscape";
-    }
+	if (window.innerHeight > window.innerWidth) {
+		return "portrait";
+	} else {
+		return "landscape";
+	}
 }
 
 function getSize() {
-    const width = window.innerWidth;
-    if (width > 1280) {
-        return "large";
-    } else if (width > 600) {
-        return " medium";
-    } else {
-        return "small";
-    }
+	const width = window.innerWidth;
+	if (width > 1280) {
+		return "large";
+	} else if (width > 600) {
+		return " medium";
+	} else {
+		return "small";
+	}
 }
 
 export default App;
